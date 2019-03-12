77aaa2c6bdbd09dca22d1a85becff99cb871a225
Igor <48448706+ZigorSK@users.noreply.github.com>
2019-03-12 03:52:56 +0300

    Create Queue_public.cpp

---
+++ Queue_public.cpp
@@ -0,0 +1,35 @@
+#include "Queue_public.h"
+
+int queue_public::task(queue &q)
+{
+	this->clear();
+	//копируем data очереди на который указывает ссылка в данную очередь
+	this->copy(q);
+
+	//подсчёт числа элементов больших среднего арифметического значения
+	node *ptr = this->get_end_ptr();
+	int count = 0, sum = 0;
+
+	float aref;
+	while (ptr)
+	{
+		count++;
+		sum += ptr->data;
+		ptr = ptr->prev_ptr;
+	}
+	aref = (int)sum / count;
+	count = 0;
+	ptr = this->get_end_ptr();
+	while (ptr)
+	{
+		if (ptr->data > aref)
+			count++;
+
+		ptr = ptr->prev_ptr;
+	}
+
+
+
+	return count;
+}
+
