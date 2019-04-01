498c3f6f6e57e79aa9c967be03c6ce9da6728d67
Igor <48448706+ZigorSK@users.noreply.github.com>
2019-04-01 03:21:51 +0300

    Update Queue.cpp

--- Queue.cpp
+++ Queue.cpp
@@ -109,13 +109,57 @@ void queue::clear()//Очищение очереди
 void queue::marge(queue & q1, queue & q2)
 {
 	this->clear();
-	//слияние элементов очереди
-	this->copy(q1);
-	this->copy(q2);
+
+	node *end_ptr_q = q1.get_end_ptr(), *ptr = nullptr;
+	int counter = 0, *array1 = nullptr;
+
+	ptr = end_ptr_q;
+	while (ptr)//Нахождение количества элементов очереди
+	{
+		counter++;
+		ptr = ptr->prev_ptr;
+	}
+	array1 = new int[counter];
+	ptr = end_ptr_q;
+	for (int i = 0; counter - i > 0; i++)//заполнение массива array1 элементами очереди, где [0] - первый элемент очереди;
+	{
+		*(array1 + counter - i - 1) = ptr->data;
+		ptr = ptr->prev_ptr;
+	}
+	for (int i = 0; counter > i; i++)
+	{
+		this->push(*(array1 + i));
+	}
+	delete[] array1;
+	array1 = nullptr;
+
+	end_ptr_q = q2.get_end_ptr();
+	ptr = nullptr;
+	counter = 0;
+
+	ptr = end_ptr_q;
+	while (ptr)//Нахождение количества элементов очереди
+	{
+		counter++;
+		ptr = ptr->prev_ptr;
+	}
+	array1 = new int[counter];
+	ptr = end_ptr_q;
+	for (int i = 0; counter - i > 0; i++)//заполнение массива array1 элементами очереди, где [0] - первый элемент очереди;
+	{
+		*(array1 + counter - i - 1) = ptr->data;
+		ptr = ptr->prev_ptr;
+	}
+	for (int i = 0; counter > i; i++)
+	{
+		this->push(*(array1 + i));
+	}
+	delete[] array1;
 }
 
 void queue::copy(queue &q1)//копируем q1 в данный
 {
+	this->clear();
 	//копирование q1 в this
 
 	node *end_ptr_q1 = q1.get_end_ptr(), *ptr = nullptr;
@@ -140,4 +184,33 @@ void queue::copy(queue &q1)//копируем q1 в данный
 	}
 	delete[] array1;
 
+}
+
+int queue::task()
+{
+	//подсчёт числа элементов больших среднего арифметического значения
+	node *ptr = this->get_end_ptr();
+	int count = 0, sum = 0;
+
+	float aref;
+	while (ptr)
+	{
+		count++;
+		sum += ptr->data;
+		ptr = ptr->prev_ptr;
+	}
+	aref = (int)sum / count;
+	count = 0;
+	ptr = this->get_end_ptr();
+	while (ptr)
+	{
+		if (ptr->data > aref)
+			count++;
+
+		ptr = ptr->prev_ptr;
+	}
+
+
+
+	return count;
 }
