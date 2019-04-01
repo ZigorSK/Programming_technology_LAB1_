f511620bfe4abaa5700e9f90b1d166aa357b9554
Igor <48448706+ZigorSK@users.noreply.github.com>
2019-04-01 03:24:46 +0300

    Update Queue_protected.h

--- Queue_protected.h
+++ Queue_protected.h
@@ -1,12 +1,14 @@
 #pragma once
 #ifndef _Queue_protected_H_
+
 #include "Queue.h"
+
 #define _Queue_protected_H_
-typedef class queue_protected : protected queue
+class queue_protected : protected queue
 {
 public:
-	int task(queue &q);
-	//Для использования методов бозового класса вне класса-наследника необходимо реализовать get-методы в нём
-	//Но так как данное ТЗ не требует этого, в даном проекте этого делаться не будет
-} queue_protected;
+	queue_protected(queue &q1);
+
+	int task_protected();
+};
 #endif
