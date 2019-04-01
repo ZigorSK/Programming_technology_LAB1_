ef9c0e3c4391d2ba4357028df67897add722e5b6
Igor <48448706+ZigorSK@users.noreply.github.com>
2019-04-01 03:21:01 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,11 +8,7 @@ int main()
 	setlocale(LC_ALL, "Rus");
 
 	queue q_base, q_copy, q_marger;
-	queue_public q_public;
-	queue_protected q_protected;
-	queue_private q_private;
-
-
+	
 	int choice = 0, n = 0, num = 0;
 
 	do
@@ -47,15 +43,21 @@ int main()
 			cin >> num;
 			switch (num)
 			{
-			case 1:
-				cout << "Колличество элементов, больших, чем среднее арифметическое:" << q_public.task(q_base) << endl;
+			case 1: {
+				queue_public q_public(q_base);
+				cout << "Колличество элементов, больших, чем среднее арифметическое:" << q_public.task() << endl;
 				break;
-			case 2:
-				cout << "Колличество элементов, больших, чем среднее арифметическое:" << q_protected.task(q_base) << endl;
+			}
+			case 2: {
+				queue_protected q_protected(q_base);
+				cout << "Колличество элементов, больших, чем среднее арифметическое:" << q_protected.task_protected() << endl;
 				break;
-			case 3:
-				cout << "Колличество элементов, больших, чем среднее арифметическое:" << q_private.task(q_base) << endl;
+			}
+			case 3: {
+				queue_private q_private(q_base);
+				cout << "Колличество элементов, больших, чем среднее арифметическое:" << q_private.task_private() << endl;
 				break;
+			}
 			default:
 				cout << "Неправильно введена команда!!!!" << endl;
 				break;
@@ -64,7 +66,6 @@ int main()
 			break;
 		case 5:
 			system("cls");
-			q_copy.clear();
 			q_copy.copy(q_base);
 			cout << "Скопированная очередь: " << endl;
 			q_copy.show();
