85aab4afa7862dabe79cca5d62bf56179d6e0dd9
Igor <48448706+ZigorSK@users.noreply.github.com>
2019-05-23 22:05:25 +0300

    Update Queue_public.h

--- Queue_public.h
+++ Queue_public.h
@@ -2,12 +2,18 @@
 #ifndef _Queue_piblic_H_
 
 #include "Queue.h"
+#include<iostream>
+
+using namespace std;
 
 #define _Queue_public_H_
- class queue_public : public queue
+class queue_public : public queue
 {
 public:
-	queue_public(queue &q);
+	queue_public();
+	int task();
+	void marge(queue_public & q1, queue_public & q2);//Перегружаем метод marge, т.к. в базовом классе аргументы типа queue
+	void copy(queue_public &q);//Перегрузка copy, аналогично marge
 };
 
 #endif
