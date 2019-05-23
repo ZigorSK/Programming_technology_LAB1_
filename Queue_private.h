2730a3355c358c90a4ab22e3bc882b119a089774
Igor <48448706+ZigorSK@users.noreply.github.com>
2019-05-23 22:03:19 +0300

    Update Queue_private.h

--- Queue_private.h
+++ Queue_private.h
@@ -1,16 +1,25 @@
 #pragma once
-#ifndef _Queue_piblic_H_
+#ifndef _Queue_private_H_
 
 #include "Queue.h"
+#include<iostream>
 
-#define _Queue_public_H_
+using namespace std;
+
+#define _Queue_private_H_
 
 class queue_private : private queue
 {
 public:
-	queue_private(queue &q1);
+	queue_private();
+	int task();
+	void marge(queue_private& q1, queue_private & q2);//Перегружаем метод marge, т.к. в базовом классе аргументы типа queue
+	void copy(queue_private &q);//Перегрузка copy, аналогично marge
+	void push_private(int d);
+	void pop_private();
+	void show_private();
+	void clear_private();
 
-	int task_private();
 };
 
 
