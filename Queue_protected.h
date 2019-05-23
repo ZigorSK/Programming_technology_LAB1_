f06e5ca7be8e9dfd657c67fb8fb644ee2bf08ff2
Igor <48448706+ZigorSK@users.noreply.github.com>
2019-05-23 22:04:33 +0300

    Update Queue_protected.h

--- Queue_protected.h
+++ Queue_protected.h
@@ -2,13 +2,21 @@
 #ifndef _Queue_protected_H_
 
 #include "Queue.h"
+#include<iostream>
+
+using namespace std;
 
 #define _Queue_protected_H_
 class queue_protected : protected queue
 {
 public:
-	queue_protected(queue &q1);
-
-	int task_protected();
+	queue_protected();
+	int task();
+	void marge(queue_protected& q1, queue_protected & q2);//Перегружаем метод marge, т.к. в базовом классе аргументы типа queue
+	void copy(queue_protected &q);//Перегрузка copy, аналогично marge
+	void push_protected(int d);
+	void pop_protected();
+	void show_protected();
+	void clear_protected();
 };
 #endif
